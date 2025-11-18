a35d2ef9237dfc1a0c92f68eaa510b5fd001b407
yeah6161 <barirampage21@gmail.com>
2025-11-18 23:47:02 +0700

    Create if_else.c

---
+++ control-flow/if_else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int value = 10;
+
+    if (value > 5)
+        printf("Value is greater than 5\n");
+    else
+        printf("Value is 5 or less\n");
+
+    return 0;
+}
